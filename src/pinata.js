7653e337b0185501bc6cfe64d3ca93954abbf213
zyy0926 <xzctjgzec7@gmail.com>
2022-08-25 09:46:30 +0000

    zyy7

--- src/pinata.js
+++ src/pinata.js
@@ -11,8 +11,9 @@ export const uploadJSONToIPFS = async(JSONBody) => {
     return axios 
         .post(url, JSONBody, {
             headers: {
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
+                // pinata_api_key: key,
+                // pinata_secret_api_key: secret,
+                authorization: "Bearer "+process.env.REACT_APP_PINATA_JWT
             }
         })
         .then(function (response) {
@@ -69,8 +70,9 @@ export const uploadFileToIPFS = async(file) => {
             maxBodyLength: 'Infinity',
             headers: {
                 'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
+                // pinata_api_key: key,
+                // pinata_secret_api_key: secret,
+                authorization: "Bearer "+process.env.REACT_APP_PINATA_JWT
             }
         })
         .then(function (response) {
